a4a84392168de784f726d6be85369062bca2f9e9
Ekta bharti <ektabharti235@gmail.com>
2024-03-13 11:17:58 +0530

    13-03

--- Day3/DataTypeExample1.java
+++ Day3/DataTypeExample1.java
@@ -1,5 +1,10 @@
 package Day3;
 
 public class DataTypeExample1 {
-    
+    public static void main(String[] args) {
+        // byte data = 45;
+        // // int data = 45;
+        // long data = 8278216l;
+        System.out.println(data);
+    }
 }
