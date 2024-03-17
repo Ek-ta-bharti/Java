6b915594ce749e5f03e8a96d54298dcc7c7e012f
Ekta bharti <ektabharti235@gmail.com>
2024-03-17 22:01:08 +0530

    17-05

--- Day5/AssignmentOperatorExample.java
+++ Day5/AssignmentOperatorExample.java
@@ -1,8 +1,20 @@
+package Day5;
 public class AssignmentOperatorExample {
     public static void main(String[] args) {
         int n1, n2, n3, n4;
         n1 = n2 = n3 = n4 = 10;
         System.out.println(n1 + " " + n2 + " " + n3 + " " + n4);
+        int a = 10;
+        a += 5;
+        System.out.println(a);
+
+        int b = 10;
+        b -= 5;
+        System.out.println(b);
+
+        int c = 10;
+        c /= 5;
+        System.out.println(c);
     }
 
 }
