ca4ca163188774697e18eb24243478e03f40dfed
Ekta bharti <ektabharti235@gmail.com>
2024-03-17 22:09:05 +0530

    17-05

---
+++ Day5/RelationOperatorExample.java
@@ -0,0 +1,16 @@
+package Day5;
+public class RelationOperatorExample {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        // boolean result = a > b;
+        // boolean result = a < b;
+        // boolean result = a == b;
+        // boolean result = a >= c;
+        // boolean result = c > b;
+        boolean result = a <= b;
+        System.out.println(result);
+    }
+
+}
