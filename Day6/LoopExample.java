1e69694dc45511adf6d188e44f5f28a66e688ac0
Ekta bharti <ektabharti235@gmail.com>
2024-03-18 13:17:54 +0530

    18-03

---
+++ Day6/LoopExample.java
@@ -0,0 +1,11 @@
+package Day6;
+public class LoopExample {
+    public static void main(String[] args) {
+        int age = 18;
+        if (age >= 19) {
+            System.out.println("you  are valid");
+        } else {
+            System.out.println("You are not valid");
+        }
+    }
+}
---
+++ Day6/LoopsOperator.java
@@ -0,0 +1,13 @@
+package Day6;
+public class LoopsOperator {
+    public static void main(String[] args) {
+        int age = 61;
+        if (age >= 18 && age < 60) {
+            System.out.println("You are an auild");
+        } else if (age >= 60) {
+            System.out.println("You are an old ");
+        } else {
+            System.out.println("Your are chlidren");
+        }
+    }
+}
