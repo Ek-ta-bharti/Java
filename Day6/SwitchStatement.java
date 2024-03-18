9946afbee08f438420f2988bfc5561af5cbbd311
Ekta bharti <ektabharti235@gmail.com>
2024-03-18 14:13:59 +0530

    18-03

---
+++ Day6/SwitchStatement.java
@@ -0,0 +1,19 @@
+package Day6;
+public class SwitchStatement {
+    public static void main(String[] args) {
+        int nums = 700;
+        switch (nums) {
+            case 200:
+                System.out.println("case1");
+                break;
+            case 300:
+                System.out.println("case2");
+                break;
+            case 400:
+                System.out.println("case3");
+                break;
+            default:
+                System.out.println("default");
+        }
+    }
+}
