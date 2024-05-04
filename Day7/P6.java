8526b65e41fac0e23c3d3caa24efc9c63780a9d1
Ekta bharti <ektabharti235@gmail.com>
2024-05-04 11:08:44 +0530

    04

--- Day7/P6.java
+++ Day7/P6.java
@@ -1,22 +1,22 @@
 
 public class P6 {
     public static void Pattern() {
-        int n = 10; 
+        int n = 10;
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (i+j == (n-1)/2) {
+                if (i + j == (n - 1) / 2) {
                     System.out.print("*");
                 } else {
                     System.out.print(" ");
                 }
             }
-            System.out.println(); 
+            System.out.println();
         }
     }
+
     public static void main(String[] args) {
         P6 square = new P6();
         square.Pattern();
     }
 }
-
