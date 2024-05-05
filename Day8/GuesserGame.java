99da8d660d15d7babaa10e9464c8cce49bca3f81
Ekta bharti <ektabharti235@gmail.com>
2024-05-05 11:29:03 +0530

    05-05

--- Day8/GuesserGame.java
+++ Day8/GuesserGame.java
@@ -2,39 +2,51 @@ import java.util.Scanner;
 
 public class GuesserGame {
 
+    private static int secretNumber;
+
     public static void guesser() {
-        int secretNumber = (int) (Math.random() * 100) + 1;
+        secretNumber = (int) (Math.random() * 10) + 1;
     }
 
-    public static void umpire() {
-
+    public static void umpire(String[] playerName, int[] playerGuess) {
+        for (int i = 0; i < playerName.length; i++) {
+            if (playerGuess[i] == secretNumber) {
+                System.out.println("Congratulations, " + playerName[i] + "! You guessed the correct number ("
+                        + secretNumber + ").");
+                return;
+            }
+        }
+        System.out.println("No one guessed the correct number (" + secretNumber + "). The game will restart.");
+        main(null); 
     }
 
     public static void main(String arr[]) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("How many Player Want To Play: ");
+        System.out.print("How many Players want to play: ");
         int playernumber = sc.nextInt();
         sc.nextLine();
         String[] playerName = new String[playernumber];
         int[] playerGuess = new int[playernumber];
 
         for (int i = 0; i < playernumber; i++) {
-            System.out.print("Enter player " + ((i + 1) + "'s name: "));
+            System.out.print("Enter player " + (i + 1) + "'s name: ");
             playerName[i] = sc.nextLine();
-            System.out.print("Enter Your Guesse Number " + playerName[i] + ": ");
+            System.out.print("Enter Your Guess Number, " + playerName[i] + ": ");
             playerGuess[i] = sc.nextInt();
             sc.nextLine();
         }
+
+        GuesserGame guesserA = new GuesserGame();
+        guesserA.guesser();
+
         System.out.println("\nPlayer Name And Player GuessNumber: ");
         for (int i = 0; i < playernumber; i++) {
             System.out.println((i + 1) + ". " + playerName[i] + "- -> " + playerGuess[i]);
         }
-        sc.close();
 
         GuesserGame umpireA = new GuesserGame();
-        GuesserGame guesserA = new GuesserGame();
-        guesserA.guesser();
-        umpireA.umpire();
-    }
+        umpireA.umpire(playerName, playerGuess);
 
+        sc.close();
+    }
 }
