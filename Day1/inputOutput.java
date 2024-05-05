688673ed0a7cd3c82072d311afaf90b9f364de7f
Ekta bharti <ektabharti235@gmail.com>
2024-05-05 11:37:14 +0530

    05

--- Day1/inputOutput.java
+++ Day1/inputOutput.java
@@ -1,13 +1,18 @@
 import java.util.*;
 
-public class inputOutput {
+public class InputOutput {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("enter your name:");
-        String name = sc.nextLine();
+        Scanner scanner = new Scanner(System.in);
+
+        // Asking for the name
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
         System.out.println("It's My Name: " + name);
-        Scanner sa = new Scanner(System.in);
+
+        // Asking for the number of players
         System.out.print("How many players want to play? ");
-        int name = sc.nextInt();
+        int numPlayers = scanner.nextInt();
+
+        scanner.close();
     }
 }
